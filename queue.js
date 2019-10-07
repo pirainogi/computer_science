83b5e1096e78ed145e4ddb097fd7ba08b47ad9e9
pirainogi <pirainogi@gmail.com>
2019-10-07 10:36:17 -0400

    catch up commit

---
+++ queue.js
@@ -0,0 +1,37 @@
+class Stack {
+  constructor(top = null){
+    this.top = top
+    this.length = top ? 0 : 1
+  }
+
+  push(data){
+    const node = new Node(data)
+  }
+
+  shift(){}
+
+  isEmpty(){
+    return !this.top
+  }
+
+  size(){
+    if(this.top === null){
+      return 0
+    } else {
+      let counter = 0
+      let node = this.top
+      while(node.next){
+        counter ++
+      }
+      return counter
+    }
+  }
+}
+
+class Node {
+  constructor(data, next=null){
+    this.data = data
+    this.next = next
+  }
+
+}
